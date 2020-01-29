2a7884e9c05565644265cbda6041c9f52c42eaf9
NITSAN Technologies <sanjay@nitsan.in>
2020-01-29 15:18:54 +0530

    [TODO] Setup demo and pro-version link

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => '[NITSAN] Revolution Slider',
-    'description' => 'World\'s most pupular revolution slider is now available in TYPO3 CMS too. Plug-n-play with Buy license https://revolution.themepunch.com/jquery/ Live-Demo: https://demo.t3terminal.com/t3t-extensions/sliders/revolution-slider/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-extensions/revolution-slider-typo3-plugin-pro/',
+    'description' => 'World\'s most pupular revolution slider is now available in TYPO3 CMS too. Plug-n-play with Buy license https://revolution.themepunch.com/jquery/ Live-Demo: https://demo.t3terminal.com/t3t-extensions/sliders/revolution-slider/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-revolution-slider-extension-pro',
     'category' => 'plugin',
     'author' => 'T3: Keval Pandya, QA: Vandna Makwana',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
