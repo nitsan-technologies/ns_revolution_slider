a23823477619d4ab2fc7e79282851d8bf558b34d
NITSAN <sanjay@nitsan.in>
2025-06-24 16:15:14 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_revolution_slider'] = [
-    'title' => 'Slider Revolution',
-    'description' => 'More than just a TYPO3 Slider! The TYPO3 Slider Revolution is developed in official partnership with Slider Revolution. It allows you to create effective sliders easily using the drag-and-drop feature. It comes with over 250+ modern slider templates and more than 2000+ pre-built slider elements, offering a wide range of transitions and effects.
+    'title' => 'TYPO3 Revolution Slider',
+    'description' => 'Create powerful and modern sliders for your TYPO3 website using the official Revolution Slider integration. Easily build sliders with drag-and-drop, 250+ templates, and 2000+ pre-built elements with advanced transitions and effects.',
 
-    *** Live Demo: https://demo.t3planet.com/t3-extensions/revolution-slider/ *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-slider-revolution-extension',
     'category' => 'plugin',
-    'author' => 'T3: Rohan Parmar, Divya Goklani, Nilesh Malankiya, QA: Krishna Dhapa',
-    'author_company' => 'T3Planet // NITSAN',
-    'author_email' => 'sanjay@nitsan.in',
+    'author' => 'Team T3Planet',
+    'author_company' => 'T3Planet',
+    'author_email' => 'info@t3planet.de',
     'state' => 'stable',
     'uploadfolder' => 1,
     'createDirs' => '',
